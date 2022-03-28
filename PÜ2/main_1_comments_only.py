fb777238e96a8ecea588d266c0c39bba96108769
am1445 <101808737+am1445@users.noreply.github.com>
2022-03-28 23:07:31 +0200

    Update main_1_comments_only.py

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,36 @@
 # UC 2.0
 
-#%% UC 2.1 Einlesen der Daten
+#%% VORBEDINGUNG: UC 1.0 Erstellen der Daten
+'''
+Daten sind vollständig vorhanden, dh.es wurde am Ergometer ein Leistungstest gemacht und aufgezeichnet
+sowie die Person im System angelegt(ID) 
 
+'''
+#%% UC 2.1 Einlesen der Daten
+'''
+Die Daten werden eingelesen 
+''' 
+#%% UC 2.2 Vorverarbeiten der Daten
+'''
+Daten werden vorverarbeitet 
+'''
+#%% UC 2.3 Analysieren der Daten auf Abbruchkriterien
+'''
+Es wird festgestellt ob die vorverarbeiteten Daten irgendwelche Abbruchkriterien erfüllen 
+'''
+#%% UC 2.4 Erstellen einer Zusammenfassung
+'''
+Eine Zusammenfassung der Daten wird erfüllt
+'''
+#%% UC 2.5 Visualisierung der Daten
+'''
+Die aufbereiteten Daten werden dargestellt 
+'''
+#%% UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums
+'''
+Die Darstellung der aufbereiteten Daten wird abgebrochen 
+'''
+#%% UC 2.7 Speichern der Daten
+'''
+Die Daten werden gespeichert 
+''' 
